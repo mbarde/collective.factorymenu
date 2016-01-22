f6b356cf43977614a831aa702b920ae2ae45b8b6
Luca Fabbri <luca@keul.it>
2016-01-22 11:03:15 +0100

    i18n

--- src/collective/factorymenu/locales/update.sh
+++ src/collective/factorymenu/locales/update.sh
@@ -9,4 +9,5 @@
 domain=collective.factorymenu
 
 i18ndude rebuild-pot --pot $domain.pot --create $domain ../
+i18ndude merge --pot $domain.pot --merge $domain-manual.pot
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
