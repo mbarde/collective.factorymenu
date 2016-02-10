e9263629067d42fca153aff7492d7d354a088b32
Luca Fabbri <luca@keul.it>
2016-02-10 12:34:51 +0100

    version is dev

--- setup.py
+++ setup.py
@@ -7,18 +7,13 @@ from setuptools import setup
 long_description = (
     open('README.rst').read() +
     '\n' +
-    'Contributors\n' +
-    '============\n' +
-    '\n' +
-    open('CONTRIBUTORS.rst').read() +
-    '\n' +
     open('CHANGES.rst').read() +
     '\n')
 
 
 setup(
     name='collective.factorymenu',
-    version='0.1.0',
+    version='0.1.0.dev0',
     description="Customize the Plone add menu",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -32,9 +27,9 @@ setup(
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
     keywords='Python Plone',
-    author='Luca Fabbri',
+    author='Luca Fabbri (keul)',
     author_email='luca@keul.it',
-    url='https://pypi.python.org/pypi/collective.factorymenu',
+    url='https://github.com/keul/collective.factorymenu',
     license='GPL version 2',
     packages=find_packages('src', exclude=['ez_setup']),
     namespace_packages=['collective'],
@@ -42,15 +37,14 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'plone.api',
-        'plone.app.contentmenu',
         'setuptools',
+        'Products.CMFPlone',
+        'plone.app.contentmenu',
     ],
     extras_require={
         'test': [
             'plone.app.testing',
-            'plone.app.contenttypes',
-            'plone.app.robotframework[debug]',
+            'plone.app.robotframework',
         ],
     },
     entry_points="""
